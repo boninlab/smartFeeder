4af82f0a4bbd25cd506c6a1e014698fda7df0c34
jhWon <55525343+boninlab@users.noreply.github.com>
2022-05-31 16:23:14 +0900

    Delete tests/api directory

--- tests/api/action-api.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import requests
-
-jobj = {'params': {'cmd': 'reset'}}
-
-response = requests.post(
-    'http://localhost:3100/feeder/system/cmd', json=jobj)
-
-print(response.status_code)
